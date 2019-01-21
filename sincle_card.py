2be8f0669a09397e62a2491b75c65c4634f237fa
a2vk6zz <scott.a.larson@gmail.com>
2019-01-21 06:10:42 -0600

    Updating messages and layout

--- sincle_card.py
+++ sincle_card.py
@@ -1,17 +1,25 @@
+
+"""
+    A very simple script to get a single Tarrot card
+    using the TarrotCard class. 
+"""
+
 import os
 from tarrotCard import getRandomTarrotCard
+from support.logos import logo4 as logo
+
 # This will only get random cards and the cards may not be unique
-
-
 
 while True:
     os.system('clear')
-    print("Welcome to the single card fortune teller")
+    print("Welcome to the single card Tarrot spread")
 
     your_card = getRandomTarrotCard()
     print("Your card is: {}".format(your_card.name))
 
-    cont = input("Get anther card(y/n)")
+    cont = input("Get anther card ('n' to exit)")
     if cont.lower() == "n":
         print("Thanks for playing")
         break
+
+print("Thank you for using...{}".format(logo))
