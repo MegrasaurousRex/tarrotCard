5ede69d266c50504c076aed863422c8a6863b0f8
a2vk6zz <scott.a.larson@gmail.com>
2019-01-27 08:50:46 -0600

    changing module name and updating code to conform to pep standards

--- tarrot_card.py
+++ tarrot_card.py
@@ -1,52 +1,54 @@
 """
     Basic Tarrot card class used to simulate a Tarrot Card or Tarrot deck
     The TarrotCard objects have no mehtods only attributes
-        
+
     Author: Scott Larson (scott.a.larson@gmail.com)
     Date: 2019-01-21
 """
+
 import random
 
 # Unsure if these should go in the TarrotCard class or not...
-major_arcana_cnames = ("The Magician", "The High Priestess", "The Empress", 
-    "The Emperor", "The Hierophant", "The Lovers", "The Chariot", 
-    "Strength", "The Hermit", "Wheel of Fortune", "Justice", 
-    "The Hanged Man", "Death", "Temperance", "The Devil", 
-    "The Tower", "The Star", "The Moon", "The Sun", "Judgement", 
-    "The World", "The Fool")
-major_arcana_ranks = ("I","II","III","IV", "V","VI","VII","VIII","IX",
-        "X", "XI","XII","XIII","XIV","XV","XVI","XVII", "XVIII","XIX",
-        "XX", "XXI",None)
-minor_arcana_cnames = ("A","2","3","4","5","6","7","8","9","10", 'Jack', 'Knight', 'Queen', 'King')
-suites = ("Cups", "Swords", "Wands", "Penticles")
+MAJOR_ARCANA_CNAMES = ("The Magician", "The High Priestess", "The Empress",
+                       "The Emperor", "The Hierophant", "The Lovers", "The Chariot",
+                       "Strength", "The Hermit", "Wheel of Fortune", "Justice",
+                       "The Hanged Man", "Death", "Temperance", "The Devil",
+                       "The Tower", "The Star", "The Moon", "The Sun", "Judgement",
+                       "The World", "The Fool")
+MAJOR_ARCANA_RANKS = ("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX",
+                      "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII",
+                      "XVIII", "XIX", "XX", "XXI", None)
+MINOR_ARCANA_CNAMES = ("A", "2", "3", "4", "5", "6", "7", "8", "9",
+                       "10", 'Jack', 'Knight', 'Queen', 'King')
+SUITES = ("Cups", "Swords", "Wands", "Pentacles")
 
 
-def getRandomTarrotCard():
+def get_random_tarrot_card():
     """Return a random Tarrot card object"""
-    major_or_minor = random.randint(0,1)
+    major_or_minor = random.randint(0, 1)
     card_choice = 0
 
     if major_or_minor == 0:
-        suite = random.choice(suites)
-        card_choice = random.randint(0,13)
+        suite = random.choice(SUITES)
+        card_choice = random.randint(0, 13)
     else:
         suite = None
-        card_choice = random.randint(0,21)
+        card_choice = random.randint(0, 21)
 
     return TarrotCard(card_choice, suite)
 
 
-def getTarrotDeck() -> list:
+def get_tarrot_deck() -> list:
     """ Return a Tarrot Deck, a python list of TarrotCard objects """
     deck = []
 
-    for suite in suites:
+    for suite in SUITES:
         for value in range(14):
             deck.append(TarrotCard(value, suite))
 
     for value in range(22):
         deck.append(TarrotCard(value, None))
-    
+
     return deck
 
 
@@ -56,22 +58,22 @@ class TarrotCard:
     def __init__(self, value, suite):
 
         if suite:
-            self.name = minor_arcana_cnames[value] + " of " + suite 
+            self.name = MINOR_ARCANA_CNAMES[value] + " of " + suite
             self.suite = suite
             self.value = value + 1
-            self.rank = minor_arcana_cnames[value]
+            self.rank = MINOR_ARCANA_CNAMES[value]
             self.arcana = "Minor"
             self.reversed = False
         else:
-            self.name = major_arcana_cnames[value] + " " + str(major_arcana_ranks[value])
+            self.name = MAJOR_ARCANA_CNAMES[value] + " " + str(MAJOR_ARCANA_RANKS[value])
             self.suite = suite
             self.value = value + 1
-            self.rank = major_arcana_ranks[value]
+            self.rank = MAJOR_ARCANA_RANKS[value]
             self.arcana = "Major"
             self.reversed = False
-        
+
         TarrotCard.card_count += 1
-        
+
     def __str__(self):
         return self.name()
 
@@ -82,7 +84,7 @@ class TarrotDeck():
 
     def __init__(self):
         self.deck = []
-        for suite in suites:
+        for suite in SUITES:
             for value in range(14):
                 self.deck.append(TarrotCard(value, suite))
                 TarrotDeck.card_count += 1
@@ -91,20 +93,18 @@ class TarrotDeck():
             self.deck.append(TarrotCard(value, None))
             TarrotDeck.card_count += 1
 
-    def shuffleDeck(self, times_to_shuffle=13):
+    def shuffle_deck(self, times_to_shuffle=13):
         '''shuffle the deck 'times_to_shuffle', default is 13 '''
-        for round in range(0,times_to_shuffle):
+        for this_round in range(0, times_to_shuffle):
             random.shuffle(self.deck)
 
-    def getASpread(self, spread_size=3):
+
+    def get_a_spread(self, spread_size=3):
         ''' Get cards for a spread, default is 3 cards '''
         self.shuffle(self.deck)
         the_spread = []
-        ### Pick UP Here: 
+        ### Pick UP Here:
         ### Need to implement getting x cards
         ### pop them to get uniqe cards
         ### return them to the deck, for future calls
         ### return the temp_deck
-
-
-
