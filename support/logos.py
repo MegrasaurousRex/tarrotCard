b5f5f75510981588baee0e079bdb462015a165b6
a2vk6zz <scott.a.larson@gmail.com>
2019-01-27 08:24:58 -0600

    added func to get random logo and code to test logos.py

--- support/logos.py
+++ support/logos.py
@@ -1,30 +1,45 @@
 """
     Silly Text Logos to display after a script ends.
     I made these on an online ascii art generator here: https://asciiartgen.now.sh/
-    If 'Scottware' is a registered trade mark no infringment is intended. 
+    If 'Scottware' is a registered trade mark no infringment is intended.
+    20190127 - pylint score of 10/10.
 """
 
-logo = """
+from random import choice
+
+LOGO = r"""
  _______ _______  _____  _______ _______ _  _  _ _______  ______ _______
  |______ |       |     |    |       |    |  |  | |_____| |_____/ |______
  ______| |_____  |_____|    |       |    |__|__| |     | |    \_ |______"""
 
-logo2 = """
+LOGO2 = r"""
 :::===  :::===== :::====  :::==== :::==== :::  ===  === :::====  :::====  :::=====
 :::     :::      :::  === :::==== :::==== :::  ===  === :::  === :::  === :::     
  =====  ===      ===  ===   ===     ===   ===  ===  === ======== =======  ======  
     === ===      ===  ===   ===     ===    ===========  ===  === === ===  ===     
 ======   =======  ======    ===     ===     ==== ====   ===  === ===  === ========"""
 
-logo3 = """
+LOGO3 = r"""
   .dBBBBP     dBBBP     dBBBBP  dBBBBBBP  dBBBBBBP     dBP   dBP  dBBBBBb   dBBBBBb     dBBBP 
   BP         dBP       dBP.BP     dBP       dBP       dBP   dBP        BB       dBP    dBP       
   `BBBBb    dBP       dBP.BP     dBP       dBP       dBPdBPdBP     dBP BB   dBBBBK    dBBP    
      dBP   dBP       dBP.BP     dBP       dBP       dBPdBPdBP     dBP  BB  dBP  BB   dBP      
 dBBBBP'   dBBBBP    dBBBBP     dBP       dBP       dBBBBBBBP     dBBBBBBB dBP  dB'  dBBBBP"""
 
-logo4 = """
+LOGO4 = r"""
   __                                        
  (_    _   _   _|_  _|_         _.  ._   _  
  __)  (_  (_)   |_   |_  \/\/  (_|  |   (/_ """
 
+LOGO_INVENTORY = [LOGO, LOGO2, LOGO3, LOGO4]
+
+def get_a_random_logo():
+    """ Return a random Logo """
+    return choice(LOGO_INVENTORY)
+
+if __name__ == "__main__":
+    print("Here are the logo's we currently have")
+    for logo in LOGO_INVENTORY:
+        print(logo)
+
+    print("\nHere is a random logo: \n{}".format(get_a_random_logo()))
