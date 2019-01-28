0be9479d95e4b325aa73d84f45556ef41e0c4332
a2vk6zz <scott.a.larson@gmail.com>
2019-01-27 22:02:51 -0600

    updating file after flake8

--- three_card_spread.py
+++ three_card_spread.py
@@ -7,11 +7,11 @@
     removed (popped) from the deck and put into the spread, another list.
 
     At the end of the reading the user is asked if they want another spread.
-    If they do the spread cards are inserted back into the deck and the loop starts
-    again.
+    If they do the spread cards are inserted back into the deck and the loop
+    starts again.
 
-    I have tried to model the deck, cards and flow of the spread as realistically
-    as possible.
+    I have tried to model the deck, cards and flow of the spread as
+    realistically as possible.
 
     Author: Scott Larson (scott.a.larson@gmail.com)
     Date: 2019-01-21
@@ -38,29 +38,35 @@ def print_card_for_spread(card_number, card, color=30):
 
     print(the_output.format(card_message, color, card.name, reversed_card))
 
+
 if __name__ == "__main__":
 
     MY_DECK = TarrotDeck()
 
     while True:
-        # randomize the cards, if a spread exists put them back and then shuffle
+        # randomize the cards, if a spread exists put them back and then
+        # shuffle
         MY_DECK.shuffle_deck(randint(13, 50))
 
         os.system('clear')
 
-        print("\033[35mWelcome to the Tarrot simple three card spread generator")
-        WAIT_FOR_USER = input("\033[36mTo create your spread press enter >>\033[0m")
+        print("\033[35mWelcome to the Tarrot simple three card spread \
+            generator")
+        WAIT_FOR_USER = \
+            input("\033[36mTo create your spread press enter >>\033[0m")
 
         # Build and display the spread, using TarrotDeck.get_a_spread() method
         MY_DECK.get_a_spread(4)
 
-        # Print the cards of the spread out for the user, function is set for 4 cards
+        # Print the cards of the spread out for the user, function is set for
+        # 4 cards
         for inc, a_card in enumerate(MY_DECK.spread):
             print_card_for_spread(inc, a_card, str(32 + inc))
 
         print("\n\033[35mPlease refer to google for the meanings of the cards and\
         \n\rhttps://thesimpletarot.com/3-card-tarot-spreads-for-beginners/\
-        \n\rfor information on the many kinds of three card spreads that exist.\033[0m")
+        \n\rfor information on the many kinds of three card spreads that \
+        exist.\033[0m")
 
         if input('Play again Enter or "n" to exit: ').lower() == 'n':
             break
