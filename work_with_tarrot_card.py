a4f8cc2dc80df481272fe4e2c2412ddaa7d4c861
a2vk6zz <scott.a.larson@gmail.com>
2019-01-27 21:17:00 -0600

    Updating notes in file.

--- work_with_tarrot_card.py
+++ work_with_tarrot_card.py
@@ -1,11 +1,19 @@
 """ 
     This file exists because I was having trouble putting all of the spread
-    cards back in the deck. A regular for loop would only put 2 cards back.
-    I had to use a while len(spread) > 0 with the for loop to make it work.
+    cards back in the deck using a for loop. A regular for loop would only put 2 cards back.
+    I had to use a while len(spread) > 0 with the for loop to make it work. this still
+    exited the for loop after the first two rounds... going to the outside while loop,
+    back into the for loop once, back out and back in (for 4 cards) totally don't get this.
     pylint complains this shouldn't be done... dunno how else to make it work.
 
     completely removed the for loop. Used the while len(spread) > 0, pop()
-    This worked.
+    This worked. But its wrong. 
+
+    https://stackoverflow.com/questions/43121340/why-is-the-use-of-lensequence-in-condition-values-considered-incorrect-by-pyli#43476778
+
+    According to stack overflow, using the sequence as a boolean is the correct approach. 
+    If there is no value the sequence evaluates to false. 
+
 """
 import sys
 sys.path.append("..")
@@ -33,3 +41,5 @@ print("Shuffeling")
 a_deck.shuffle_deck(13)
 print_deck_len(a_deck)
 print_spread_len(a_deck)
+
+a_deck.get_a_spread(20)
